1e2b54cc20009f4806227f47750b04f82d67acd7
Daniel <roderm99@gmail.com>
2024-01-25 11:52:00 -0500

    add password validity check

---
+++ script.js
@@ -0,0 +1,38 @@
+const pass = document.querySelector('#password');
+const confPass = document.querySelector("#confirm-password");
+
+
+function confirmPassword(password) {
+    password = pass.value;
+
+    if (password !== confPass.value) {
+        alert("Passwords Do Not Match.");
+    }
+
+}
+
+function checkPasswords(){
+    
+    if (pass.value !== confPass.value) {
+        pass.parentNode.appendChild(errorMessage);
+    } else {
+        pass.parentNode.removeChild(errorMessage);
+    }
+}
+
+let errorMessage = document.createElement("div");
+errorMessage.textContent = "* Passwords do not match";
+errorMessage.style.color = "red";
+errorMessage.style.fontFamily = `"Caveat", cursive, Arial, Helvetica, sans-serif;`;
+errorMessage.style.fontSize = "1rem";
+errorMessage.style.marginLeft = "";
+
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', event => {
+    confirmPassword();
+})
+
+pass.addEventListener('input', checkPasswords);
+confPass.addEventListener('input', checkPasswords);
+
